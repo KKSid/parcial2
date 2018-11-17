9a5013f8e8f81ae2f08f722a84958ce3c33a0745
KKSid <32003435+KKSid@users.noreply.github.com>
2018-11-16 20:19:50 -0800

    inventario
    parcial 2 prg grafica

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+  <login></login>
+  <Ussuarios></Ussuarios>
+  </div>
+</template>
+
+<script>
+import login from './components/login.vue'
+import Ussuarios from './components/Ussuarios.vue'
+export default{
+  name: 'app',
+  components:{
+    login,
+    Ussuarios
+  }
+}
+</script>
